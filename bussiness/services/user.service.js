761c49062e06f11746f31477af11b9f1af3211cb
Thai Hoc Nguyen <hocnt@techvn.mti.co.jp>
2021-08-10 22:39:47 +0700

    ADD: populate for user model

--- bussiness/services/user.service.js
+++ bussiness/services/user.service.js
@@ -1,22 +1,22 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import nodemailer from 'nodemailer';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
 
-import User from '../../models/user.js';
-import jwtGenerator from '../../api/security/jwtGenerator.js';
-import logInValidator from '../../api/validators/logInValidator.js';
-import registerValidator from '../../api/validators/registerValidator.js';
-import updateOneUserValidator from '../../api/validators/updateOneUserValidator.js';
-import userRepository from '../../data/repositories/user.repository.js';
-import roleRepository from '../../data/repositories/role.repository.js';
-import entityRepository from '../../data/repositories/entity.repository.js';
-import logInResponseEnum from '../../utils/enums/logInResponseEnum.js';
-import registerResponseEnum from '../../utils/enums/registerResponseEnum.js';
-import updateOneUserResponseEnum from '../../utils/enums/updateOneUserResponseEnum.js';
-import jwtEnum from '../../utils/enums/jwtEnum.js';
-import userResponseEnum from '../../utils/enums/userResponseEnum.js';
-import roleResponseEnum from '../../utils/enums/roleResponseEnum.js';
-import userValidator from '../../api/validators/userValidator.js';
+import User from "../../models/user.js";
+import jwtGenerator from "../../api/security/jwtGenerator.js";
+import logInValidator from "../../api/validators/logInValidator.js";
+import registerValidator from "../../api/validators/registerValidator.js";
+import updateOneUserValidator from "../../api/validators/updateOneUserValidator.js";
+import userRepository from "../../data/repositories/user.repository.js";
+import roleRepository from "../../data/repositories/role.repository.js";
+import entityRepository from "../../data/repositories/entity.repository.js";
+import logInResponseEnum from "../../utils/enums/logInResponseEnum.js";
+import registerResponseEnum from "../../utils/enums/registerResponseEnum.js";
+import updateOneUserResponseEnum from "../../utils/enums/updateOneUserResponseEnum.js";
+import jwtEnum from "../../utils/enums/jwtEnum.js";
+import userResponseEnum from "../../utils/enums/userResponseEnum.js";
+import roleResponseEnum from "../../utils/enums/roleResponseEnum.js";
+import userValidator from "../../api/validators/userValidator.js";
 
 const _entityRepository = entityRepository(User);
 
@@ -25,19 +25,20 @@ const userService = {
     // validate request
     try {
       const resultValidator = logInValidator(email, password);
-      if (resultValidator.code !== logInResponseEnum.SUCCESS) return resultValidator;
+      if (resultValidator.code !== logInResponseEnum.SUCCESS)
+        return resultValidator;
       // Find email in DB
       const user = await userRepository.getOneByEmail(email);
       if (!user) {
         return {
-          code: logInResponseEnum.WRONG_EMAIL
+          code: logInResponseEnum.WRONG_EMAIL,
         };
       }
       // Comparate password from DB and password from request
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) {
         return {
-          code: logInResponseEnum.WRONG_PASSWORD
+          code: logInResponseEnum.WRONG_PASSWORD,
         };
       }
       // Generate Token
@@ -52,29 +53,35 @@ const userService = {
         email: user.email,
         code: logInResponseEnum.SUCCESS,
         accessToken: resultJwtGenerator.accessToken,
-        refreshToken: resultJwtGenerator.refreshToken
+        refreshToken: resultJwtGenerator.refreshToken,
       };
     } catch (e) {
       return {
-        code: logInResponseEnum.SERVER_ERROR
-      }
+        code: logInResponseEnum.SERVER_ERROR,
+      };
     }
   },
   async register(email, name, password, rePassword) {
     try {
       // validate request
-      const resultValidator = registerValidator(email, name, password, rePassword);
-      if (resultValidator.code !== registerResponseEnum.SUCCESS) return resultValidator;
+      const resultValidator = registerValidator(
+        email,
+        name,
+        password,
+        rePassword
+      );
+      if (resultValidator.code !== registerResponseEnum.SUCCESS)
+        return resultValidator;
       // Find email in DB
       let user = await userRepository.getOneByEmail(email);
       if (user) {
         return {
-          code: registerResponseEnum.EMAIL_IS_UNAVAILABLE
+          code: registerResponseEnum.EMAIL_IS_UNAVAILABLE,
         };
       }
       if (password !== rePassword) {
         return {
-          code: registerResponseEnum.PASSWORD_DOES_NOT_MATCH
+          code: registerResponseEnum.PASSWORD_DOES_NOT_MATCH,
         };
       }
 
@@ -85,14 +92,12 @@ const userService = {
           email: email,
           name: name,
           role: role.id,
-          password: password
+          password: password,
         },
       };
-      const token = jwt.sign(
-        payload,
-        process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: "10m",
-        });
+      const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10m",
+      });
       const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 587,
@@ -104,7 +109,7 @@ const userService = {
       });
       const url = `http://localhost:5000/api/user-controller/confirmation/${token}`;
       const mailOption = {
-        from: 'huskarit99@gmail.com',
+        from: "huskarit99@gmail.com",
         to: `${name} <${email}>`,
         subject: "Confirmation Email",
         html: `<h1>Email Confirmation</h1>
@@ -124,12 +129,12 @@ const userService = {
       });
 
       return {
-        code: registerResponseEnum.SUCCESS
-      }
+        code: registerResponseEnum.SUCCESS,
+      };
     } catch (e) {
       return {
-        code: registerResponseEnum.SERVER_ERROR
-      }
+        code: registerResponseEnum.SERVER_ERROR,
+      };
     }
   },
   async confirmEmail(token) {
@@ -139,12 +144,17 @@ const userService = {
       const exp = decoded.exp;
       if (dateNow >= exp * 1000) {
         return {
-          code: jwtEnum.TOKEN_IS_EXPIRED
-        }
+          code: jwtEnum.TOKEN_IS_EXPIRED,
+        };
       } else {
         const salt = await bcrypt.genSalt(10);
         const password = await bcrypt.hash(decoded.user.password, salt);
-        const user = new User({ email: decoded.user.email, name: decoded.user.name, password: password, role: decoded.user.role });
+        const user = new User({
+          email: decoded.user.email,
+          name: decoded.user.name,
+          password: password,
+          role: decoded.user.role,
+        });
         console.log(user);
         await _entityRepository.addOne(user);
         return {
@@ -154,15 +164,19 @@ const userService = {
     } catch (e) {
       console.log(e);
       return {
-        code: registerResponseEnum.SERVER_ERROR
-      }
+        code: registerResponseEnum.SERVER_ERROR,
+      };
     }
   },
   async getSelfInfo(id) {
-    const user = await _entityRepository.getOneById(id);
+    const user = await userRepository.getOneById(id);
     return {
-      user
-    }
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role.name,
+      },
+    };
   },
 
   async getAllByRoleName(role_name, page) {
@@ -170,78 +184,90 @@ const userService = {
       const role = await roleRepository.getOneByName(role_name);
       if (!role) {
         return {
-          code: roleResponseEnum.ROLE_NAME_IS_UNAVAILABLE
-        }
+          code: roleResponseEnum.ROLE_NAME_IS_UNAVAILABLE,
+        };
       }
       const users = await userRepository.getAllByRoleId(role._id);
 
       // Pagination
       let tmp = [];
       const page_number = [];
-      let _i = 0
+      let _i = 0;
       for (var i = 0; i < users.length; i++) {
         if (Math.floor(_i / 5) == page - 1) {
           const data = users[_i];
           tmp.push(data);
         }
         if (_i / 5 == Math.floor(_i / 5)) {
-          page_number.push((_i / 5) + 1);
+          page_number.push(_i / 5 + 1);
         }
         _i++;
       }
       return {
         code: userResponseEnum.SUCCESS,
         users: tmp,
-        page_number
-      }
+        page_number,
+      };
     } catch (e) {
       return {
-        code: userResponseEnum.SERVER_ERROR
-      }
+        code: userResponseEnum.SERVER_ERROR,
+      };
     }
   },
 
   async updateOneById(request) {
     try {
       if (request.status) {
-        await _entityRepository.updateOneById(request.id, { status: request.status, refresh_token: '', refresh_token_expiry_time: 0 });
+        await _entityRepository.updateOneById(request.id, {
+          status: request.status,
+          refresh_token: "",
+          refresh_token_expiry_time: 0,
+        });
       } else {
-        await _entityRepository.updateOneById(request.id, { status: request.status });
+        await _entityRepository.updateOneById(request.id, {
+          status: request.status,
+        });
       }
       return {
-        code: userResponseEnum.SUCCESS
-      }
+        code: userResponseEnum.SUCCESS,
+      };
     } catch (e) {
       return {
-        code: userResponseEnum.SERVER_ERROR
-      }
+        code: userResponseEnum.SERVER_ERROR,
+      };
     }
   },
   async addOne(email, name) {
     try {
       // validate request
       const resultValidator = userValidator(email, name);
-      if (resultValidator.code !== userResponseEnum.VALIDATOR_IS_SUCCESS) return resultValidator;
+      if (resultValidator.code !== userResponseEnum.VALIDATOR_IS_SUCCESS)
+        return resultValidator;
       let user = await userRepository.getOneByEmail(email);
       if (user) {
         return {
-          code: userResponseEnum.EMAIL_IS_UNAVAILABLE
-        }
+          code: userResponseEnum.EMAIL_IS_UNAVAILABLE,
+        };
       }
       const salt = await bcrypt.genSalt(10);
-      const password = await bcrypt.hash('12345678', salt);
+      const password = await bcrypt.hash("12345678", salt);
       const role = await roleRepository.getOneByName("teacher");
-      user = new User({ email: email, name: name, role: role._id, password: password });
+      user = new User({
+        email: email,
+        name: name,
+        role: role._id,
+        password: password,
+      });
       await _entityRepository.addOne(user);
       return {
-        code: userResponseEnum.SUCCESS
-      }
+        code: userResponseEnum.SUCCESS,
+      };
     } catch (e) {
       return {
-        code: userResponseEnum.SERVER_ERROR
-      }
+        code: userResponseEnum.SERVER_ERROR,
+      };
     }
-  }
-}
+  },
+};
 
 export default userService;
