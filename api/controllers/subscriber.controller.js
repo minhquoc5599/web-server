991b081704f691bfbf145fac595528b1e00e914d
Thai Hoc Nguyen <hocnt@techvn.mti.co.jp>
2021-08-14 23:57:04 +0700

    MOD: fix get user id from token

--- api/controllers/subscriber.controller.js
+++ api/controllers/subscriber.controller.js
@@ -1,72 +1,66 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import httpStatusCode from '../../utils/enums/httpStatusCode.js';
-import subscriberService from '../../bussiness/services/subscriber.service.js';
-import subscriberResponseEnum from '../../utils/enums/subscriberResponseEnum.js';
-import jwt from 'jsonwebtoken'
-import auth from '../middlewares/auth.js';
+import httpStatusCode from "../../utils/enums/httpStatusCode.js";
+import subscriberService from "../../bussiness/services/subscriber.service.js";
+import subscriberResponseEnum from "../../utils/enums/subscriberResponseEnum.js";
+import jwt from "jsonwebtoken";
+import auth from "../middlewares/auth.js";
 
 const router = Router();
-router.post('/subscribe', auth(['student']), async(req, res) => {
+router.post("/subscribe", auth(["student"]), async (req, res) => {
   const { course_id } = req.body;
-  const accessToken = req.cookies['access_token'];
-  let user = null;
-  if (accessToken) {
-    const decoded = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-    user = decoded.user;
-  };
-  const student_id = user.id;
-  const result = await subscriberService.subscribe(course_id, student_id)
+  const student_id = req.user.id;
+  const result = await subscriberService.subscribe(course_id, student_id);
   if (result.code !== subscriberResponseEnum.SUCCESS) {
-    return res.status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST).send(result).end();
+    return res
+      .status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST)
+      .send(result)
+      .end();
   }
   res.status(httpStatusCode.SUCCESS.CREATED).json(result).end();
 });
 
-router.get('/subscribers/:id', auth(['student']), async(req, res) => {
+router.get("/subscribers/:id", auth(["student"]), async (req, res) => {
   const course_id = req.params.id;
-  const accessToken = req.cookies['access_token'];
-  let user = null;
-  if (accessToken) {
-    const decoded = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-    user = decoded.user;
-  };
-  const result = await subscriberService.getAllByCourseId(course_id, user);
+  const result = await subscriberService.getAllByCourseId(course_id, req.user);
   if (result.code !== subscriberResponseEnum.SUCCESS) {
-    return res.status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST).send(result).end();
+    return res
+      .status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST)
+      .send(result)
+      .end();
   }
   res.status(httpStatusCode.SUCCESS.OK).json(result).end();
 });
 
-router.get('/subscribers', auth(['student']), async(req, res) => {
+router.get("/subscribers", auth(["student"]), async (req, res) => {
   const page = Number(req.query.page) || 1;
-  const accessToken = req.cookies['access_token'];
-  let user = null;
-  if (accessToken) {
-    const decoded = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-    user = decoded.user;
-  };
-  const student_id = user.id
+  const student_id = req.user.id;
   const result = await subscriberService.getAllByStudentId(student_id, page);
   if (result.code !== subscriberResponseEnum.SUCCESS) {
-    return res.status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST).send(result).end();
+    return res
+      .status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST)
+      .send(result)
+      .end();
   }
   res.status(httpStatusCode.SUCCESS.OK).json(result).end();
 });
 
-router.put('/rating', auth(['student']), async(req, res) => {
+router.put("/rating", auth(["student"]), async (req, res) => {
   const { course_id, rating, comment } = req.body;
-  const accessToken = req.cookies['access_token'];
-  let user = null;
-  if (accessToken) {
-    const decoded = await jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
-    user = decoded.user;
-  };
-  const student_id = user.id;
+  const student_id = req.user.id;
   const name = user.name;
-  const result = await subscriberService.rating(course_id, student_id, rating, comment, name);
+  const result = await subscriberService.rating(
+    course_id,
+    student_id,
+    rating,
+    comment,
+    name
+  );
   if (result.code !== subscriberResponseEnum.SUCCESS) {
-    return res.status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST).send(result).end();
+    return res
+      .status(httpStatusCode.CLIENT_ERRORS.BAD_REQUEST)
+      .send(result)
+      .end();
   }
   res.status(httpStatusCode.SUCCESS.OK).json(result).end();
 });
